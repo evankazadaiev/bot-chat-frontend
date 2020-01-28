9e83af0f2763d9268955d68356ec9a1ae25f05a6
Ivan <ivan.kazadaev@technorely.com>
2020-01-28 12:13:13 +0200

    updated server

--- server.js
+++ server.js
@@ -16,24 +16,48 @@
 // app.listen(port, () => {
 //   console.log(`listening on port: ${port}`);
 // });
+// const express = require('express');
+// const path = require('path');
+// const history = require('connect-history-api-fallback');
+// // ^ middleware to redirect all URLs to index.html
+//
+// const app = express();
+// const staticFileMiddleware = express.static(path.join(__dirname));
+// const port = process.env.PORT || 8080;
+//
+// app.use(staticFileMiddleware);
+// app.use(history());
+// app.use(staticFileMiddleware);
+// // ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
+//
+// app.get('/', (req, res) => {
+//   res.render(path.join(`${__dirname}/index.html`));
+// });
+//
+// app.listen(port, () => {
+//   console.log(`Express serving on ${port}!`);
+// });
+
+const port = process.env.PORT || 8080;
 const express = require('express');
 const path = require('path');
 const history = require('connect-history-api-fallback');
-// ^ middleware to redirect all URLs to index.html
 
 const app = express();
-const staticFileMiddleware = express.static(path.join(__dirname));
-const port = process.env.PORT || 8080;
+
+const staticFileMiddleware = express.static(path.join(`${__dirname}/dist`));
 
 app.use(staticFileMiddleware);
-app.use(history());
+app.use(history({
+  disableDotRule: true,
+  verbose: true,
+}));
 app.use(staticFileMiddleware);
-// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
 
 app.get('/', (req, res) => {
-  res.render(path.join(`${__dirname}/index.html`));
+  res.render(path.join(`${__dirname}/dist/index.html`));
 });
 
-app.listen(port, () => {
-  console.log(`Express serving on ${port}!`);
+app.listen(process.env.PORT || 8080, () => {
+  console.log('App now running on port', port);
 });
